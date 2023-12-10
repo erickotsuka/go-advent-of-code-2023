3b2065fc09174d0a1775bf3665787412934f7bb7
Erick Otsuka <erick.r.otsuka@gmail.com>
2023-12-09 23:38:16 -0300

    :sparkles: Add day 7 part 2 solution

--- aoc.go
+++ aoc.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	solutions.Day8Part2()
+	solutions.Day7Part2()
 }
--- solutions/day7.go
+++ solutions/day7.go
@@ -149,3 +149,168 @@ func Day7Part1() {
 	fmt.Println(result)
 
 }
+
+func Day7Part2() {
+	// cards := []rune{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
+	cardToStrengthMap := make(map[rune]rune, 13)
+
+	cardToStrengthMap['J'] = '0'
+	cardToStrengthMap['2'] = '1'
+	cardToStrengthMap['3'] = '2'
+	cardToStrengthMap['4'] = '3'
+	cardToStrengthMap['5'] = '4'
+	cardToStrengthMap['6'] = '5'
+	cardToStrengthMap['7'] = '6'
+	cardToStrengthMap['8'] = '7'
+	cardToStrengthMap['9'] = '8'
+	cardToStrengthMap['T'] = '9'
+	cardToStrengthMap['Q'] = 'A' 
+	cardToStrengthMap['K'] = 'B'
+	cardToStrengthMap['A'] = 'C'
+
+	handToBidMap := make(map[string]int)
+	handTypeToHandsMap := make(map[handType][]string)
+
+	result := 0
+
+	lines := strings.Split(utils.ReadInput(7), "\n")
+
+	for _, line := range lines {
+		handAndBid := strings.Split(line, " ")	
+		hand := handAndBid[0]
+		bid, err := strconv.Atoi(handAndBid[1])
+		if err != nil {
+			panic(err)
+		}
+
+		handToBidMap[hand] = bid
+
+		cardCountMap := make(map[rune]int)
+		jokersCount := 0
+
+		for _, card := range hand {
+			if card == 'J' {
+				jokersCount++
+				continue
+			}
+			cardCountMap[card] = cardCountMap[card] + 1	
+		}
+
+		var handType handType
+
+		if jokersCount == 0 {
+			switch len(cardCountMap) {
+			case 1:
+				handType = fiveOfAKind
+			case 2:
+				for _, count := range cardCountMap {
+					if count == 1 || count == 4 {
+						handType = fourOfAKind
+					} else {
+						handType = fullHouse
+					}
+					break
+				}
+			case 3:
+				for _, count := range cardCountMap {
+					if count == 1 {
+						continue
+					}
+					if count == 3 {
+						handType = threeOfAKind
+						break
+					} 
+					if count == 2 {
+						handType = twoPair
+						break
+					}
+				}
+			case 4:
+				handType = onePair
+			case 5:
+				handType = highCard
+			}
+		} else if jokersCount == 1 {
+			switch len(cardCountMap) {
+			case 1:
+				handType = fiveOfAKind
+			case 2:
+				for _, count := range cardCountMap {
+					if count == 2 {
+						handType = fullHouse
+					} else {
+						handType = fourOfAKind
+					}
+					break
+				}
+			case 3:
+				handType = threeOfAKind
+			case 4:
+				handType = onePair
+			}
+		} else if jokersCount == 2 {
+			switch len(cardCountMap) {
+			case 1:
+				handType = fiveOfAKind
+			case 2:
+				handType = fourOfAKind
+			case 3:
+				handType = threeOfAKind	
+			}
+		} else if jokersCount == 3 {
+			switch len(cardCountMap) {
+			case 1:
+				handType =  fiveOfAKind
+			case 2:
+				handType = fourOfAKind
+			}
+		} else if jokersCount == 4 {
+			handType = fiveOfAKind
+		} else if jokersCount == 5 {
+			handType = fiveOfAKind
+		}
+
+
+		hands, handTypeExists := handTypeToHandsMap[handType]
+		if handTypeExists {
+			hands = append(hands, hand)	
+		} else {
+			hands = []string{hand} 
+		}
+		handTypeToHandsMap[handType] = hands 
+
+	}
+
+	for handType, hands := range handTypeToHandsMap {
+		handStrenghtStrings := make([]string, len(hands))
+		handStrenghtStringToHandMap := make(map[string]string, len(hands))
+		for index, hand := range hands {
+			handStrengthString := ""
+			for _, card := range hand {
+				handStrengthString = handStrengthString + string(cardToStrengthMap[card])
+			}
+			handStrenghtStringToHandMap[handStrengthString] = hand
+			handStrenghtStrings[index] = handStrengthString
+		}
+		slices.Sort(handStrenghtStrings)
+		sortedHands := make([]string, len(hands))
+		for index, handStrenghtString := range handStrenghtStrings {
+			sortedHands[index] = handStrenghtStringToHandMap[handStrenghtString]
+		}
+		handTypeToHandsMap[handType] = sortedHands
+	}
+
+	weakestToStrongestHandTypes := []handType{ highCard, onePair, twoPair, threeOfAKind, fullHouse, fourOfAKind, fiveOfAKind }
+	
+	currentRank := 1
+	for _, handType := range weakestToStrongestHandTypes {
+		for _, hand := range handTypeToHandsMap[handType] {
+			bid := handToBidMap[hand]
+			result += bid * currentRank
+			currentRank++
+		}	
+	}
+
+	fmt.Println(result)
+
+}
